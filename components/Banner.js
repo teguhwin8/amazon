783008393a68c43e12257bda853b69d5a78f4083
Teguh <teguhwin8@gmail.com>
2022-12-13 21:00:23 +0700

    banner component

---
+++ components/Banner.js
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+
+function Banner() {
+  return (
+    <div className="relative">
+      <div className="absolute w-full h-32 bg-gradient-to-t from-white to-transparent bottom-0 z-20"></div>
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        interval={5000}
+      >
+        <div className="relative aspect-[21/9]">
+          <Image
+            src="/images/amazon-banner-1.jpeg"
+            alt="Amazon Banner 1"
+            loading="lazy"
+            fill
+          />
+        </div>
+        <div className="relative aspect-[21/9]">
+          <Image
+            src="/images/amazon-banner-2.jpg"
+            alt="Amazon Banner 2"
+            loading="lazy"
+            fill
+          />
+        </div>
+        <div className="relative aspect-[21/9]">
+          <Image
+            src="/images/amazon-banner-3.jpg"
+            alt="Amazon Banner 3"
+            loading="lazy"
+            fill
+          />
+        </div>
+      </Carousel>
+    </div>
+  );
+}
+
+export default Banner;
